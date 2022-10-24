e3f260b91c3c8b84fa96e34355ada98267e1ae99
Marco <patrickstar1281@yahoo.com>
2022-10-24 05:57:43 -0500

    updating tests

---
+++ src/component/Nav/__tests__/index.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+afterEach(cleanup);
+
+describe('Nav component', () => {
+    it('renders', () => {
+        render(<Nav />);
+    });
+
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Nav />);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
+
+describe('emoji is visiable', () => {
+    it('inserts emoji into the h2', () => {
+        
+    })
+})
