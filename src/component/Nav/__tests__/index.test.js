fde1b3baa97d607cb375417122906afde5e873df
Marco <patrickstar1281@yahoo.com>
2022-10-25 00:22:06 -0500

    added some tests for the nav component

--- src/component/Nav/__tests__/index.test.js
+++ src/component/Nav/__tests__/index.test.js
@@ -6,10 +6,12 @@ import Nav from '..';
 afterEach(cleanup);
 
 describe('Nav component', () => {
+    //baseline test
     it('renders', () => {
         render(<Nav />);
     });
 
+    //snapshot test
     it('matches snapshot', () => {
         const { asFragment } = render(<Nav />);
 
@@ -19,6 +21,17 @@ describe('Nav component', () => {
 
 describe('emoji is visiable', () => {
     it('inserts emoji into the h2', () => {
-        
-    })
-})
+        const { getByLabelText } = render(<Nav />);
+
+        expect(getByLabelText('camera')).toHaveTextContent('📸');
+    });
+});
+
+describe('links are visible', () => {
+    it('inserts text into the links', () => {
+        const { getByTestId } = render(<Nav />);
+
+        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+        expect(getByTestId('about')).toHaveTextContent('About Me');
+    });
+});
