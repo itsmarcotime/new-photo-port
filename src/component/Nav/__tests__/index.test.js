7354089b154abd92d1a22d6bb3cc797eb7e79198
Marco <patrickstar1281@yahoo.com>
2022-10-30 11:23:42 -0500

    added some tests to contact

--- src/component/Nav/__tests__/index.test.js
+++ src/component/Nav/__tests__/index.test.js
@@ -8,6 +8,8 @@ const categories = [
 ]
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
@@ -18,6 +20,8 @@ describe('Nav component', () => {
             categories={categories}
             SetCurrentCategory={mockSetCurrentCategory}
             currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
             />);
     });
 
@@ -27,6 +31,8 @@ describe('Nav component', () => {
             categories={categories}
             SetCurrentCategory={mockSetCurrentCategory}
             currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
             />);
 
         expect(asFragment()).toMatchSnapshot();
@@ -39,6 +45,8 @@ describe('emoji is visiable', () => {
             categories={categories}
             SetCurrentCategory={mockSetCurrentCategory}
             currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
             />);
 
         expect(getByLabelText('camera')).toHaveTextContent('📸');
@@ -51,9 +59,11 @@ describe('links are visible', () => {
             categories={categories}
             SetCurrentCategory={mockSetCurrentCategory}
             currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
             />);
 
         expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-        expect(getByTestId('about')).toHaveTextContent('About Me');
+        expect(getByTestId('about')).toHaveTextContent('About me');
     });
 });
